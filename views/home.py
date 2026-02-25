f939977d1e9d20cd8014060ddfebb11eb7c45205
kipisha <selvakip@students.zhaw.ch>
2026-02-25 10:15:00 +0100

    name hinzugefügt

--- views/home.py
+++ views/home.py
@@ -6,8 +6,9 @@ st.title("Meine erste Streamlit App")
 
 """
 Diese App wurde von folgenden Personen entwickelt:
+- Darlene Armenio (armdar01@students.zhaw.ch)
 - Kipisha Selvan  (selvakip@students.zhaw.ch)
-
+- 
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
