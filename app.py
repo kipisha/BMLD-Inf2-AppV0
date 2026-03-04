5dd081dd671df873001395b33a9367ef710eb06f
kipisha <selvakip@students.zhaw.ch>
2026-03-04 13:02:12 +0100

    n

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 
-st.set_page_config(page_title="Meine App", page_icon=":material/home:")
+# Wir definieren nur die Seiten, die ihr WIRKLICH im Ordner 'views' habt
+pg = st.navigation([
+    st.Page("views/home.py", title="Home", icon="🏠"),
+    st.Page("views/bakterien_rechner.py", title="Bakterien-Rechner", icon="🧫"),
+    st.Page("views/unterseite_a.py", title="Zusatz-Rechner", icon="📊")
+])
 
-# Wenn die Datei im Hauptverzeichnis liegt:
-pg_second = st.Page("addition_calculator.py", title="Unterseite A", icon=":material/info:")
-
-# Wenn die Datei in einem UNTERORDNER namens "addition_calculator" liegt:
-pg_second = st.Page("addition_calculator/addition_calculator.py", title="Unterseite A", icon=":material/info:")
-
-
-pg = st.navigation([pg_home, pg_second])
+# Das hier startet die App
 pg.run()
