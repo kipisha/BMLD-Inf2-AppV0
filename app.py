608770ac31feaa956a5c41d821c8ff011dfb48ce
kipisha <selvakip@students.zhaw.ch>
2026-03-04 10:35:26 +0100

    our app calculator

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ import streamlit as st
 
 st.set_page_config(page_title="Meine App", page_icon=":material/home:")
 
-pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
+# Wenn die Datei im Hauptverzeichnis liegt:
 pg_second = st.Page("addition_calculator.py", title="Unterseite A", icon=":material/info:")
+
+# Wenn die Datei in einem UNTERORDNER namens "addition_calculator" liegt:
+pg_second = st.Page("addition_calculator/addition_calculator.py", title="Unterseite A", icon=":material/info:")
+
 
 pg = st.navigation([pg_home, pg_second])
 pg.run()
---
+++ functions/calculations.py
@@ -0,0 +1,22 @@
+import math
+
+def calculate_bacterial_growth(n0,t,g):
+    if g <= 0:
+        return 0
+    
+    n_gen= t /g 
+    nt = n0* (2**n_gen)
+
+    return nt, n_gen
+
+def get_growth_steps(n0,t,g, steps=10):
+
+    times_points = []
+    counts = []
+
+    for i in range(steps + 1 ):
+        current_t = (t/steps) * i 
+        nt_step,_ = calculate_bacterial_growth(n0,current_t,g)
+        times_points.append(int(current_t))
+        counts.append(int(nt_step))
+        
