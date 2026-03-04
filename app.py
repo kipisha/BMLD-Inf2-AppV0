2fac5f9cded900b84c295b7f19291efa3cbabcd2
kipisha <selvakip@students.zhaw.ch>
2026-03-04 13:06:37 +0100

    c

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
-# Wir definieren nur die Seiten, die ihr WIRKLICH im Ordner 'views' habt
+
 pg = st.navigation([
     st.Page("views/home.py", title="Home", icon="🏠"),
     st.Page("views/bakterien_rechner.py", title="Bakterien-Rechner", icon="🧫"),
     st.Page("views/unterseite_a.py", title="Zusatz-Rechner", icon="📊")
 ])
 
-# Das hier startet die App
 pg.run()
