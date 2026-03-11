818a89583b394ca5ba94f33eeeff22ad6c14f2e8
kipisha <selvakip@students.zhaw.ch>
2026-03-11 10:19:35 +0100

    calculation angepasst

--- functions/calculations.py
+++ functions/calculations.py
@@ -1,4 +1,6 @@
 import math
+from datetime import datetime
+import pytz
 
 def calculate_bacterial_growth(n0,t,g):
     if g <= 0:
@@ -16,8 +18,19 @@ def get_growth_steps(n0,t,g, steps=10):
 
     for i in range(steps + 1 ):
         current_t = (t/steps) * i 
-        nt_step,_ = calculate_bacterial_growth(n0,current_t,g)
+        nt_step_ = calculate_bacterial_growth(n0,current_t,g)
         times_points.append(int(current_t))
         counts.append(int(nt_step))
-    return times_points, counts 
-        
+    return {
+        "timestamp": datetime.now(pytz.timezone('Europe/Zurich')),
+        “initial_counts“: n0,
+        “total_time“: t,
+        “growth_rate“: g, 
+        “final_count“: counts[-1], 
+        “data: {
+            “times“: times_points,
+            “counts“: counts
+
+                }
+
+    }
